3121d41f2d2d47603d8d4f63399183b27d313b0c
Sera <h.kaeru.s1321@gmail.com>
2026-05-10 09:39:32 +0900

    Initial Commit

---
+++ ScreenLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenLens
+//
+//  Created by h S. on 2026/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenLens/ScreenLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenLensApp.swift
+//  ScreenLens
+//
+//  Created by h S. on 2026/05/10.
+//
+
+import SwiftUI
+
+@main
+struct ScreenLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
